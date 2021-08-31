4d5dd2e2d1a62aa852d065965d3f63dfef3d6820
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-31 10:51:29 +0100

    Set up other inputs and outputs

--- src/main.cpp
+++ src/main.cpp
@@ -22,17 +22,48 @@ Red                   D11
 */
 
 #include <Arduino.h>
-#include "MarioFanfare.h"
 
-#define SPEAKER_PIN  7
+#define BLUE_LEDS      3
+#define GREEN_LEDS     4
+#define YELLOW_LEDS    5
+#define RED_LEDS       6
+#define SPEAKER_PIN    7
+#define BLUE_BUTTON    8
+#define GREEN_BUTTON   9
+#define YELLOW_BUTTON  10
+#define RED_BUTTON     11
 
-MarioFanfare marioFanfare(SPEAKER_PIN);
+int red_val = 1;
 
 void setup() {
-  Serial.begin(9600);  // start serial to PC
-  marioFanfare.startPlayback();
+  // Start serial connection to PC
+  Serial.begin(9600);
+
+  // Set the mode of all the pins we're using
+  pinMode(BLUE_LEDS, OUTPUT);
+  pinMode(GREEN_LEDS, OUTPUT);
+  pinMode(YELLOW_LEDS, OUTPUT);
+  pinMode(RED_LEDS, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
+  pinMode(BLUE_BUTTON, INPUT_PULLUP);
+  pinMode(GREEN_BUTTON, INPUT_PULLUP);
+  pinMode(YELLOW_BUTTON, INPUT_PULLUP);
+  pinMode(RED_BUTTON, INPUT_PULLUP);
+
+  // Write initial values to outputs
+  digitalWrite(BLUE_LEDS, LOW);
+  digitalWrite(GREEN_LEDS, LOW);
+  digitalWrite(YELLOW_LEDS, LOW);
+  digitalWrite(RED_LEDS, LOW);
+  noTone(SPEAKER_PIN);
 }
 
 void loop() {
-  marioFanfare.update();
+  int new_red_val = digitalRead(RED_BUTTON);
+  if (new_red_val != red_val) {
+    red_val = new_red_val;
+    String msg = "Button changed to ";
+    msg.concat(red_val);
+    Serial.println(msg);
+  }
 }
